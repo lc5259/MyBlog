b9efc5cb791a8ad77f0442d618d33f7dea9aae54
lc <1433420541@qq.com>
2024-01-05 17:12:28 +0800

    提交SysMenuService

--- Server/MyBlog.Server/MyBlog.Server.Application/Menu/SysMenuService.cs
+++ Server/MyBlog.Server/MyBlog.Server.Application/Menu/SysMenuService.cs
@@ -1,7 +1,24 @@
-﻿using Microsoft.OpenApi.Extensions;
+﻿using EasyCaching.Core;
+using Microsoft.OpenApi.Extensions;
+using MyBlog.Server.Application.Auth;
+using MyBlog.Server.Core.Entities;
 
 namespace MyBlog.Server.Application.Menu;
 /// <summary>
 /// 系统菜单管理
 /// </summary>
-public class SysMenuService { }
+public class SysMenuService : BaseService<SysMenu>, ITransient
+{
+    private readonly IRepository<SysMenu> _repository;
+    private readonly IEasyCachingProvider _easyCachingProvider;
+    private readonly AuthManager _authManager;
+
+    public SysMenuService(IRepository<SysMenu> repository,
+        IEasyCachingProvider easyCachingProvider,
+        AuthManager authManager) : base(repository)
+    {
+        this._repository = repository;
+        this._easyCachingProvider = easyCachingProvider;
+        this._authManager = authManager;
+    }
+}
